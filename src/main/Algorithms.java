7ea10aecbadd19c299e66d4b2107ce6d5cfdfca1
fis-lab <hishor3@gmail.com>
2025-04-22 09:28:30 +0200

    renamed files

--- src/main/Algorithms.java
+++ src/main/Algorithms.java
@@ -1,7 +1,6 @@
-package Main.algorithms;
+package algorithms;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class Algorithms {
---
+++ src/test/AlgorithmsTest.java
@@ -0,0 +1,22 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AlgorithmsTest {
+    @Test
+    public void testTwoSum() {
+        //Arrange
+        algorithms.Algorithms algorithms = new algorithms.Algorithms();
+        int[]nums = new int[]{2,7,11,15};
+        int target = 9;
+        int[] expected = new int[]{0,1};
+
+        //Act
+        int[] result = algorithms.twoSum(nums,target);
+
+        //Assert
+        assertEquals(expected,result);
+    }
+}

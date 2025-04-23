e65f74f67782025fddf86696f87fb19a7f080e3c
fis-lab <hishor3@gmail.com>
2025-04-23 14:45:36 +0200

    Implemented BinarySearch

--- src/algorithms/search/Search.java
+++ src/algorithms/search/Search.java
@@ -1,4 +1,22 @@
 package algorithms.search;
 
 public class Search {
+    public boolean binarySearch(int[] array, int number) {
+        int high = array.length;
+        int low = 0;
+        do {
+            int middle = low + (high-low)/2;
+            if (array[middle] == number) {
+                return true;
+            }
+            if (array[middle] < number) {
+                low = middle + 1;
+            }
+            if (array[middle] > number) {
+                high = middle - 1;
+            }
+
+        } while (low <= high);
+        return false;
+    }
 }

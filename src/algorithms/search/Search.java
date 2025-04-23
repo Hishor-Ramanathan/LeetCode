3f03ec68e14645eb2737c07676f6b8705613b42d
fis-lab <hishor3@gmail.com>
2025-04-23 15:16:15 +0200

    Implemented twoCrystalBall

--- src/algorithms/search/Search.java
+++ src/algorithms/search/Search.java
@@ -5,7 +5,7 @@ public class Search {
         int high = array.length;
         int low = 0;
         do {
-            int middle = low + (high-low)/2;
+            int middle = low + (high - low) / 2;
             if (array[middle] == number) {
                 return true;
             }
@@ -18,5 +18,28 @@ public class Search {
 
         } while (low <= high);
         return false;
+    }
+
+    /*
+    Given two crystal ball that will break if dropped from a high enough distance,
+    determine the exact spot in which it will break in the most  optimized way.
+     */
+    public int twoCrystalBall(boolean[] floors) {
+        int jmpAmount = Float.floatToIntBits(Math.round(Math.sqrt(floors.length)));
+        int i = 0;
+        for (; i < floors.length; i += jmpAmount) {
+            if (floors[i]) {
+                break;
+            }
+        }
+
+        i -= jmpAmount;
+
+        for (int j = i; j < floors.length; j++) {
+            if (floors[j]) {
+                return j + 1;
+            }
+        }
+        return -1;
     }
 }
